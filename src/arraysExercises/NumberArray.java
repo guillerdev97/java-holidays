77f410b0555b31f259a3a2567c49ea6e21c7fbcc
Guillermo García <guillermieres97@gmail.com>
2022-08-02 12:59:43 +0200

    array exercise looping through array using for and for each loop

---
+++ src/arraysExercises/NumberArray.java
@@ -0,0 +1,19 @@
+package arraysExercises;
+
+public class NumberArray {
+    public static void main(String[] args) {
+        int[] studentsAge = {12, 21, 22, 32, 12};
+        int[] studentsMoney = new int[5];
+        studentsMoney[0] = 2345;
+        studentsMoney[1] = 334;
+        studentsMoney[2] = 453;
+        studentsMoney[3] = 44456;
+        studentsMoney[4] = 2342;
+        for(int index = 0; index < studentsAge.length; index++) {
+            System.out.println(studentsAge[index]);
+        }
+        for(int age : studentsMoney) {
+            System.out.println(age);
+        }
+    }
+}
