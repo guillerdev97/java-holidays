677cd2573a25544833e8bc03b90659a284563482
Guillermo García <guillermieres97@gmail.com>
2022-08-04 09:02:37 +0200

    adding implementation to the exercise

--- src/loopsExercises/PrintRangeNumbers.java
+++ src/loopsExercises/PrintRangeNumbers.java
@@ -13,10 +13,15 @@ public class PrintRangeNumbers {
 
     public static void printNumbers(int num) {
         int sum = 0;
+        int evenSum = 0;
         for(int index = 1; index <= num; index++) {
             System.out.println(index);
             sum += index;
+            if(index % 2 == 0) {
+                evenSum += index;
+            }
         }
         System.out.println("Sum result: " + sum);
+        System.out.println("Sum of even numbers: " + evenSum);
     }
 }
